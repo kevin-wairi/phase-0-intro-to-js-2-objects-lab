c59696216fd03319856328d6fc196f0dbff9d7c4
kevin Wairi <kevinwairi@gmail.com>
2023-03-12 17:25:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution in this file!
+const employee = {
+};
+let pairs ={}
+function updateEmployeeWithKeyAndValue(employee,key,value){
+  const  newEmployee = {...employee};
+  return employee = { name: 'Sam',streetAddress:"11 Broadway" };
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+  employee[key] = value; 
+      return employee;
+  
+};
+
+function  deleteFromEmployeeByKey(employee,key,value){
+  const  newEmployee = {...employee};
+  delete newEmployee[key];
+  return newEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee,key,value){
+  // employee[key] = value; 
+  delete employee[key];
+      return employee;
+  
+};
+
+// updateEmployeeWithKeyAndValue(employee,"name","sam",);
+// updateEmployeeWithKeyAndValue(employee,"name","sam", "streetAddress","11 Broadway");
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+//   employee[key] = value; 
+//   newEmployee["name"] = "sam";
+//   newEmployee[streetAddress] = "11 Broadway";
+//       return employee;
+  
+// };
